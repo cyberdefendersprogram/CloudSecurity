e8fcb4b1cbcc81c8b4b6306fb60b7b696d4aeb8c
lavenderLatte <tohanara@gmail.com>
2018-07-30 16:51:50 -0700

    cleaned up the code

--- tool_1.0/publicBucketChecker.py
+++ tool_1.0/publicBucketChecker.py
@@ -1,104 +1,107 @@
 import boto3
 from collections import defaultdict
 
-access_key = ''
-secret_key = ''
-report_path = "/tmp/report.txt"
 
-GROUPS_TO_CHECK = {
-    "http://acs.amazonaws.com/groups/global/AllUsers": "Everyone",
-    "http://acs.amazonaws.com/groups/global/AuthenticatedUsers": "Authenticated AWS users"
-}
-numBuckets = 0
-numPublicBuckets = 0
-
+##################[ METHODS ]##################
 
 def printWelcomeMessage():
-    msg = 'Welcome to the Snow Cloud Program!' + '\n'
-    print(msg)
+    msg = '\n' + '|*||*||*||*||*| Welcome to the Snow Cloud S3 Bucket Checker |*||*||*||*||*|' + '\n'
+    return msg
 
 
-def printNumBuckets():
-    msg = 'The TOTAL number of buckets is: ' + str(numBuckets)
-    print(msg)
+def printNumBuckets(): 
+    msg = 'The total number of buckets: ' + str(numBuckets)
+    return msg
 
 
 def printNumPublicBuckets():
-    msg = 'The TOTAL number of PUBLIC buckets is: ' + str(numPublicBuckets)
-    print(msg)
+	# @!!!!! maybe can can add a message why they need to be carefule with public bucket 
+    msg = 'The total number of PUBLIC buckets: ' + str(numPublicBuckets) + '\n'
+    return msg
 
 
-def add_to_output(msg):
-    print(msg)
+def getCredentials():
+    accessKey = input("AWS Access Key ID?: ")
+    secretKey = input("AWS Secret Access Key?: ")
+    return [accessKey, secretKey]
 
 
 def get_location(bucket_name, s3_client):
     """
     Returns the bucket location.
-    :param bucket_name: Name of the bucket.
-    :param s3_client: s3_client instance.
-    :return: String with bucket's region.
+    - INPUT: bucket_name: Name of the bucket, s3_client: s3_client instance.
+    - OUTPUT: loc(str): a bucket's region
     """
-    loc = s3_client.get_bucket_location(
-            Bucket=bucket_name)["LocationConstraint"]
+    loc = s3_client.get_bucket_location(Bucket = bucket_name)["LocationConstraint"]
+    
     if loc is None:
-        loc = "None(probably Northern Virginia)"
+        loc = "NONE"
+    
     return loc
 
 
 def check_acl(acl):
     """
     Checks if the Access Control List is public.
-    :param acl: Acl instance that describes bucket's.
-    :return: Bucket's public indicator and dangerous grants parsed from acl instance.
+    - INPUT: Acl instance
+    - OUTPUT: is_public(boolean): public or not, 
     """
     dangerous_grants = defaultdict(list)
+    
     for grant in acl.grants:
         grantee = grant["Grantee"]
         if grantee["Type"] == "Group" and grantee["URI"] in GROUPS_TO_CHECK:
             dangerous_grants[grantee["URI"]].append(grant["Permission"])
-    public_indicator = True if dangerous_grants else False
-    return public_indicator, dangerous_grants
+    
+    is_public = True if dangerous_grants else False 
+    
+    return is_public
 
 
-def getCredentials():
-    accessKey = input("AWS Access Key ID?: ")
-    secretKey = input("AWS Secret Access Key?: ")
-    print('')
-    return [accessKey, secretKey]
+##################[ MAIN ]##################
 
-# Main Method
+GROUPS_TO_CHECK = {
+    "http://acs.amazonaws.com/groups/global/AllUsers": "Everyone",
+    "http://acs.amazonaws.com/groups/global/AuthenticatedUsers": "Authenticated AWS users"
+}
 
+numBuckets = 0
+numPublicBuckets = 0
 
-printWelcomeMessage()
+# Start with welcome message 
+print(printWelcomeMessage())
 
+# Get credentials to access s3 account
 keys = getCredentials()
+print('\n' + '–––––––––––––––– Scannning.... ––––––––––––––––' + '\n')
 access_key = keys[0]
 secret_key = keys[1]
 
-s3 = boto3.resource("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-s3_client = boto3.client("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+# Define S3 as the AWS service that we are goign to use
+s3 = boto3.resource("s3", aws_access_key_id = access_key, aws_secret_access_key = secret_key)
+# Provide credentials to boto3
+s3_client = boto3.client("s3", aws_access_key_id = access_key, aws_secret_access_key = secret_key)
 
+# Get all s3 buckets 
 buckets = s3.buckets.all()
-msg = ''
 
+# Get info of each bucket
 for bucket in buckets:
+    # get location of bucket 
     location = get_location(bucket.name, s3_client)
-    # add_to_output(report_path)
+    # retrieve the policy grant info
     bucket_acl = bucket.Acl()
-    public, grants = check_acl(bucket_acl)
+    public = check_acl(bucket_acl)
     numBuckets += 1
 
+    # render message
     if public:
-        if report_path:
-            msg = "Bucket {}: {}".format(bucket.name, "PUBLIC! VERY BAD! :(")
-            numPublicBuckets += 1
+        msg = "Bucket {}: {}".format(bucket.name, "PUBLIC! VERY BAD! :(")
+        numPublicBuckets += 1
     else:
-        if report_path:
-            msg = "Bucket {}: {}".format(bucket.name, "NOT PUBLIC! Good!")
+        msg = "Bucket {}: {}".format(bucket.name, "NOT PUBLIC! Good!")
     print(msg)
-    print("Location: {}".format(location))
-    print('')
+    print("Location: {} \n".format(location))
 
-printNumBuckets()
-printNumPublicBuckets()
+print(printNumBuckets())
+print(printNumPublicBuckets())
