b7cbb9fb8ae83d8992149e14e56430c99b52b2ff
Jeremy <j3jeremy@gmail.com>
2018-08-06 17:34:05 -0700

    Added Documentation of code

--- tool_1.0/publicBucketChecker.py
+++ tool_1.0/publicBucketChecker.py
@@ -8,6 +8,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
+# Prints to the console and writes to the file
+# Input: msg_to_print: (str) message, line_down (boolean) if you need to print a carriage return
 def printAndWrite(msg_to_print, line_down):
     print(msg_to_print, end='')
     f.write(msg_to_print)
@@ -15,52 +17,51 @@ def printAndWrite(msg_to_print, line_down):
         print('')
         f.write('\n')
 
+
+# Returns welcome message at the very beginning
 def printWelcomeMessage():
     msg = '\n' + '|*||*||*||*||*| Welcome to the Snow Cloud AWS Checker |*||*||*||*||*|' + '\n'
     return msg
 
-
+# Returns Thank You message
 def thankYouMessage():
     return 'Thank you for using our snow white program!'
 
 
+# Returns the total number of bucket message
 def printNumBuckets():
     msg = 'The total number of buckets:         ' + str(numBuckets)
     return msg
 
 
+# Returns the number of public buckets message
 def printNumPublicBuckets():
     # @!!!!! maybe can can add a message why they need to be carefule with public bucket
     msg = 'The total number of PUBLIC buckets:  ' + str(numPublicBuckets) + '\n'
     return msg
 
 
+# Requests the user for AWS Access Key and Secret Key
+# Returns Access Key and Secret Key in array
 def getCredentials():
     accessKey = input("AWS Access Key ID?: ")
     secretKey = input("AWS Secret Access Key?: ")
     return [accessKey, secretKey]
 
 
+# Input: bucket_name: Name of bucket, s3_client: s3_client instance
+# Returns the bucket location
 def get_location(bucket_name, s3_client):
-    """
-    Returns the bucket location.
-    - INPUT: bucket_name: Name of the bucket, s3_client: s3_client instance.
-    - OUTPUT: loc(str): a bucket's region
-    """
     loc = s3_client.get_bucket_location(Bucket=bucket_name)["LocationConstraint"]
-
     if loc is None:
         loc = "NONE"
-
     return loc
 
 
+# Checks if the Access Control List is public.
+# Input: Acl instance
+# Returns boolean if public or not
 def check_acl(acl):
-    """
-    Checks if the Access Control List is public.
-    - INPUT: Acl instance
-    - OUTPUT: is_public(boolean): public or not,
-    """
     dangerous_grants = defaultdict(list)
 
     for grant in acl.grants:
@@ -72,6 +73,9 @@ def check_acl(acl):
 
     return is_public
 
+
+# Prints the last activity date and how many days the the s3 bucket has been inactive for
+# Input: s3_client: s3 instance, bucket_name: name of s3 bucket in AWS
 def s3_last_used(s3_client, bucket_name):
     last_activity = 'N/A'
     object_list = s3_client.list_objects_v2(Bucket=bucket_name, FetchOwner=True)
@@ -92,6 +96,9 @@ def s3_last_used(s3_client, bucket_name):
             printAndWrite(str(days_str), True)
 
 
+# Looks between either access key and sees if either or them are activated
+# Input: credreport: credit report, index: the index within the credit report
+# Return: True or False. True if active. False if inactive
 def root_keys_active(credreport, index):
     if (credreport[index]['access_key_1_active'] == 'true') or (credreport[index]['access_key_2_active'] == 'true'):
         return True
@@ -99,6 +106,8 @@ def root_keys_active(credreport, index):
         return False
 
 
+# Generates and retrieves a credit report from AWS
+# Return: report as dictionary item
 def get_cred_report():
     x = 0
     try:
@@ -137,6 +146,8 @@ def get_cred_report():
 
 ##################[ MAIN ]##################
 
+
+# Initial information for email setup
 host = "smtp.gmail.com"
 port = 587
 username = "cloudreport.sw@gmail.com"
@@ -152,6 +163,7 @@ GROUPS_TO_CHECK = {
     "http://acs.amazonaws.com/groups/global/AuthenticatedUsers": "Authenticated AWS users"
 }
 
+# Variables Needed
 numBuckets = 0
 numPublicBuckets = 0
 msg = ''
@@ -176,6 +188,7 @@ iam_client = boto3.client("iam", aws_access_key_id=access_key, aws_secret_access
 buckets = s3.buckets.all()
 
 # Get info of each bucket
+# Checks if each bucket is public or not and prints if that is true
 for bucket in buckets:
     location = get_location(bucket.name, s3_client)
     # add_to_output(report_path)
@@ -196,6 +209,7 @@ for bucket in buckets:
     s3_last_used(s3_client, bucket.name)
     printAndWrite('', True)
 
+# Prints total number of buckets and the total number of public buckets
 printAndWrite(printNumBuckets(), True)
 printAndWrite(printNumPublicBuckets(), True)
 printAndWrite('', True)
@@ -204,12 +218,19 @@ printAndWrite('', True)
 
 printAndWrite('- - - - - - - - - - IAM Report - - - - - - - - - - \n', True)
 
+# Retrieve credit report
 cred_report = get_cred_report()
 
 num_users = cred_report.__len__()
 printAndWrite('The number of Users (Including the Root User) is: ', False)
 printAndWrite(str(num_users) + '\n', True)
 
+# Loop through all of the IAM Users and print out
+# 1. User name
+# 2. Access Key Active or not
+# 3. User creation Date
+# 4. Password last used date
+# 5. MFA Active or not
 for i in range(0, num_users):
     # User Name and Whether or not the Key is active
     printAndWrite('User ' + str(i + 1) + ':      ', False)
@@ -240,26 +261,38 @@ for i in range(0, num_users):
     printAndWrite('             MFA Active:             ' + cred_report[i]['mfa_active'], True)
     printAndWrite('', True)
 
+printAndWrite('# # # # # # # # # # SUMMARY # # # # # # # # # #', True)
+printAndWrite('', True)
+
+# Print if root access keys are active or not
 if root_keys_active(cred_report, 0):
     printAndWrite('Root Access Keys Active ! ! !', True)
 else:
     printAndWrite('No Root Access Keys.', True)
 
-# Email Portion of tool
+# Implement Hana's code here in summary section
+
+# Email Portion of tool -------------------------------------------------------------
 
 printAndWrite('', True)
 
 plain_txt = ''
 
-with open('report.txt', 'r') as f:
-    for line in f:
-        plain_txt += line
-
+# Ask user if they would like a report sent to them via email
+print('\n')
 ans = input('Would You like this report to be emailed to you? (Y/N): ')
 print('')
 
+# if the user input length is greater than 0, that means they inputted something
 if ans.__len__() > 0:
+    # If the first character is 'Y' or 'y' then the input is yes
     if ans[:1] == 'Y' or ans[:1] == 'y':
+
+        # Add contents from report.txt file to plain_txt (str)
+        with open('report.txt', 'r') as f:
+            for line in f:
+                plain_txt += line
+
         to_list[0] = input('Please enter your email: ')
         print('')
         try:
@@ -283,4 +316,6 @@ if ans.__len__() > 0:
         except smtplib.SMTPException:
             print('Error sending message! Did not send :(')
 
+# Print thank you message
 print(thankYouMessage())
+f.close()
